fce613af5ba4e831aa0c5b6efe84754be8164b47
naman-toshniwal <109726889+naman-toshniwal@users.noreply.github.com>
2024-06-10 10:23:19 +0530

    dsa

---
+++ Medium/Nuts and Bolts problem.py
@@ -0,0 +1,26 @@
+"""
+Given a set of n nuts & bolts. There is a one-on-one mapping between nuts and bolts. You have to Match nuts and bolts efficiently. Comparison of a nut to another nut or a bolt to another bolt is not allowed. It means the nut can only be compared with the bolt and the bolt can only be compared with the nut to see which one is bigger/smaller.
+The elements should follow the following order: { !,#,$,%,&,*,?,@,^ }
+
+Note: Make all the required changes directly in the given arrays, output will be handled by the driver code.
+"""
+
+class Solution:
+    def matchPairs(self, n, nuts, bolts):
+        return nuts.sort(), bolts.sort()
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        nuts = list(map(str, input().strip().split()))
+        bolts = list(map(str, input().strip().split()))
+        ob = Solution()
+        ob.matchPairs(n, nuts, bolts)
+        for x in nuts:
+            print(x, end=" ")
+        print()
+        for x in bolts:
+            print(x, end=" ")
+        print()
+        tc -= 1
