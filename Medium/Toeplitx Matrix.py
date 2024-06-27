d258c53ecc29e7aa2d7aa04a25f028b0d0817f6a
naman-toshniwal <109726889+naman-toshniwal@users.noreply.github.com>
2024-06-27 10:45:27 +0530

    dsa

---
+++ Medium/Toeplitx Matrix.py
@@ -0,0 +1,33 @@
+"""
+A Toeplitz (or diagonal-constant) matrix is a matrix in which each descending diagonal from left to right is constant, i.e., all elements in a diagonal are the same. Given a rectangular matrix mat, your task is to complete the function isToeplitz which returns true if the matrix is Toeplitz otherwise, it returns false.
+"""
+
+def isToepliz(mat):
+    for i in range(1, len(mat)):
+        for j in range(1, len(mat[0])):
+            if mat[i][j] != mat[i-1][j-1]:
+                return False
+    return True
+
+#{ 
+ # Driver Code Starts
+# Your code goes here
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n, m = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        matrix = [[0 for i in range(m)] for j in range(n)]
+        k = 0
+        for i in range(n):
+            for j in range(m):
+                matrix[i][j] = arr[k]
+                k += 1
+        b = isToepliz(matrix)
+
+        if b == True:
+            print("true")
+        else:
+            print("false")
+
+# } Driver Code Ends
