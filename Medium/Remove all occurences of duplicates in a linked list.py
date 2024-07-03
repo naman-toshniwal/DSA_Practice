e0abf55d08352b36006947fa277c111d7b76666d
naman-toshniwal <109726889+naman-toshniwal@users.noreply.github.com>
2024-07-03 10:17:48 +0530

    dsa

---
+++ Medium/Remove all occurences of duplicates in a linked list.py
@@ -0,0 +1,83 @@
+"""
+Given a sorted linked list, delete all nodes that have duplicate numbers (all occurrences), leaving only numbers that appear once in the original list, and return the head of the modified linked list. 
+"""
+
+"""
+# Linked list Node class
+
+    class Node :
+        def __init__(self, val):
+            self.data = val
+            self.next = None
+
+"""
+
+class Solution:
+    def removeAllDuplicates(self, head):
+        dummy = Node(0)
+        dummy.next = head
+        current = dummy
+        
+        while current.next and current.next.next:
+            if current.next.data == current.next.next.data:
+                duplicate_value = current.next.data
+                
+                while current.next and current.next.data == duplicate_value:
+                    current.next = current.next.next
+            else:
+                current = current.next
+        
+        return dummy.next
+
+import atexit
+import io
+import sys
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+class Node:
+    def __init__(self, data):  # data -> value stored in node
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+    def printList(self, head):
+        if head is None:
+            print(' ')
+            return
+        curr_node = head
+        while curr_node:
+            print(curr_node.data, end=" ")
+            curr_node = curr_node.next
+        print(' ')
+
+if __name__ == '__main__':
+    t = int(input())
+    for cases in range(t):
+        N = int(input())
+        a = LinkedList()  # create a new linked list 'a'.
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        ob = Solution()
+        head = ob.removeAllDuplicates(a.head)
+        a.printList(head)
