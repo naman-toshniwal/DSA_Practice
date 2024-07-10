900317c1d19a268a60a6b599daac8ba302084667
naman-toshniwal <109726889+naman-toshniwal@users.noreply.github.com>
2024-07-10 18:20:50 +0530

    dsa

---
+++ Medium/Largest square formed in a matrix.py
@@ -0,0 +1,53 @@
+"""
+Given a binary matrix mat of size n * m, find out the maximum length of a side of a square sub-matrix with all 1s.
+"""
+
+from typing import List
+
+class Solution:
+    def maxSquare(self, n : int, m : int, mat : List[List[int]]) -> int:
+        memo = {}
+        
+        def dp(i,j):
+            if i >= n or j >= m:
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if mat[i][j] == 1:
+                ans = min(dp(i+1,j), dp(i,j+1), dp(i+1,j+1)) + 1
+                memo[(i,j)] = ans
+                return ans
+            memo[(i,j)] = 0
+            return 0
+            
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                ans = max(ans, dp(i,j))
+    
+        return ans
+
+class IntMatrix:
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n, m):
+        matrix = []
+        for _ in range(n):
+            matrix.append([int(i) for i in input().strip().split()])
+        return matrix
+
+    def Print(self, arr):
+        for i in arr:
+            for j in i:
+                print(j, end=" ")
+            print()
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n, m = map(int, input().split())
+        mat = IntMatrix().Input(n, m)
+        obj = Solution()
+        res = obj.maxSquare(n, m, mat)
+        print(res)
